3cdb95ffdbb1c2a2d6987741e06a676a2924b745
Carla N. Pérez <69050594+carlaperez9@users.noreply.github.com>
2024-04-04 15:20:29 -0400

    test: get request

--- insert_data.py
+++ insert_data.py
@@ -14,26 +14,28 @@ dummy_data = [
 # { "input": "dummy"}
 
 def insert_dummy_data():
-    try:
+    # try:
         # # Define the API endpoint URL
-        # api_url = "http://localhost:3000"        
+    api_url = "http://localhost:8080/test"        
+        
+    get_request = requests.get_request(api_url)
 
         # Iterate over dummy data and send POST requests
-        for data in dummy_data:
+    #     for data in dummy_data:
 
-            api_url = "http://localhost:8080/" + data["email"]
-            input_data = { "input": data["input"]}
-            # Send a POST request to the API endpoint
-            response = requests.post(api_url, json=input_data)
+    #         api_url = "http://localhost:8080/" + data["email"]
+    #         input_data = { "input": data["input"]}
+    #         # Send a POST request to the API endpoint
+    #         response = requests.post(api_url, json=input_data)
 
-            # Check if the request was successful
-            if response.status_code == 200:
-                print("Data inserted successfully.")
-            else:
-                print("Failed to insert data. Server responded with status code:", response.status_code)
+    #         # Check if the request was successful
+    #         if response.status_code == 200:
+    #             print("Data inserted successfully.")
+    #         else:
+    #             print("Failed to insert data. Server responded with status code:", response.status_code)
 
-    except Exception as e:
-        print("Failed to insert data:", e)
+    # except Exception as e:
+    #     print("Failed to insert data:", e)
 
 if __name__ == "__main__":
     insert_dummy_data()
