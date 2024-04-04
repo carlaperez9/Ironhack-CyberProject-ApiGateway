97e7bb911f851596c8d53089cf82085f659e84a8
Carla Perez Irizarry <carla.perezirizarry@popular.com>
2024-04-04 10:54:15 -0400

    feat: added skeleton rest api using nodejs and express

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express'); 
+const app = express(); 
+const PORT = 8080; 
+
+app.listen(
+    PORT, 
+    () => console.log(`it's alive on http://localhost:${PORT}`)
+)
+
+app.use( express.json() )
+ 
+app.get('/tshirt', (req, res) => { 
+    res.status(200).send({
+        tshirt: 'tshirt',
+        size: 'large'
+    })
+}); 
+
+app.post('/tshirt/:id', (req, res) => { 
+    const { id }   = req.params; 
+    const { logo } = req.body; 
+    
+    if (!logo){ 
+        res.status(418).send({ message: 'we need a logo!' })
+    }
+
+    res.send({ 
+        tshirt: `t-shirt with your ${logo} and ID of ${id}`, 
+    })
+});
